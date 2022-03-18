48dc4bfd4931aebf27f4cd2d7b968c257e5b4af7
Hp <oluwafisayomiogunyemi@gmail.com>
2022-03-18 18:16:07 +0100

    added a Carousel slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:chelsea_app/navigation_drawer.dart';
 import 'package:flutter/material.dart';
 
@@ -7,10 +10,23 @@ void main() {
   ));
 }
 
+final List<String> imgList = [
+  'assets/images/draw.jpg'
+      'assets/images/tuchel.jpg'
+      'assets/images/azpili.jpg'
+      'assets/images/coaches.jpg'
+];
+
+final List<String> news = [
+  'Date confirmed for Real Madrid Ties'
+      'Three players uncertain for tie against Boro'
+      'Azpilicueta pleased with his contributions to the team'
+      'Tuchel reacts to the draw'
+];
+
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       drawer: NavigationDrawer(),
       appBar: AppBar(
@@ -18,7 +34,56 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue[400],
       ),
-      
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Latest Chelsea News",
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
+            ),
+            CarouselSlider(
+              items: imgList
+                  .map((item) => Container(
+                        child: Image.asset(
+                          item,
+                          fit: BoxFit.cover,
+                          width: 1000,
+                        ),
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                autoPlay: true,
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+  
+  
+/* Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      drawer: NavigationDrawer(),
+      appBar: AppBar(
+        title: const Text("Chelsea App"),
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
+      ),
+      body: Center(
+        child: CarouselSlider.builder(
+            itemCount: , itemBuilder: itemBuilder, options: options),
+      ),
+    ); 
+ */
